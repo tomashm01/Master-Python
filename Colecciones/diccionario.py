613053568825ca9bf724d2abc977fe19870eef1a
tomas <tomashidalgomartin@gmail.com>
2021-07-03 18:10:48 +0200

    Diccionarios

---
+++ Colecciones/diccionario.py
@@ -0,0 +1,11 @@
+#Poseen la llave y su valor
+diccionario={
+  "IDE": "Integrated Development Environment",
+  "OOP":"Object oriented programming",
+  "DBMS": "Database Management System"
+}
+print(diccionario)
+diccionario["PK"]="Primary Key"
+print(diccionario)
+diccionario.pop("IDE")
+print(diccionario)
