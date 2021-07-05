b7a6079e35e20b16ccbed0ded6a1d4f61298c2ce
Tomás Hidalgo <tomashidalgomartin@gmail.com>
2021-07-06 00:25:39 +0200

    Delete Catalogo_peliculas directory

--- Catalogo_peliculas/Pelicula.py
+++
@@ -1,9 +0,0 @@
-class Pelicula:
-  def __init__(self,nombre):
-    self.__nombre=nombre
-  def __str__(self) -> str:
-    return "El nombre es: "+self.__nombre
-  def get_nombre(self):
-    return self.__nombre
-  def set_nombre(self,nombre):
-    self.__nombre=nombre
