56ba6fdd73755b2f7501f19c3de570918bd05d49
Tomás Hidalgo <tomashidalgomartin@gmail.com>
2021-07-06 00:25:54 +0200

    Delete POO-Avanzado directory

--- POO-Avanzado/color.py
+++
@@ -1,3 +0,0 @@
-class Color:
-  def __init__(self,color):
-    self.__color=color
--- POO-Avanzado/cuadrado.py
+++
@@ -1,9 +0,0 @@
-from figura_geometrica import Figura
-from color import Color
-
-class Cuadrado(Figura,Color):
-  def __init__(self,lado,color):
-    Figura.__init__(self,lado,lado)
-    Color.__init__(self,color)
-  def area(self):
-    return super().__base*super().__altura
