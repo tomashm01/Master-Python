d6258b0dfc0a01be572377a33081572b2fc8553a
Tomás Hidalgo <tomashidalgomartin@gmail.com>
2021-07-05 13:37:11 +0200

    Delete Bases de Datos directory

--- Bases de Datos/conexionPrueba.py
+++
@@ -1,8 +0,0 @@
-import pymysql as sql
-conexion=sql.connect(host='localhost',user='root',passwd='',db='python')
-cursor=conexion.cursor()
-cursor.execute("select nombre from persona where id<30")
-for nombre in cursor.fetchall():
-  print(nombre)
-conexion.close()
-cursor.close()
